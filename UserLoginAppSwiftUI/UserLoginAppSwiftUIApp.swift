b2195de4d00d0f04177cf028ea6175e9bfbb329e
EfimovSchool <sergio-251@yandex.ru>
2022-01-08 14:09:36 +0300

    Initial Commit

---
+++ UserLoginAppSwiftUI/UserLoginAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserLoginAppSwiftUIApp.swift
+//  UserLoginAppSwiftUI
+//
+//  Created by Sergey Efimov on 08.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct UserLoginAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
